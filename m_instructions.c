03b3ab19c23320c08e2ea13a4253e405fa675dec
jchois <choisjuliana@gmail.com>
2020-05-14 13:35:04 -0500

    condition to call de functions

--- m_instructions.c
+++ m_instructions.c
@@ -5,14 +5,31 @@
  * @op: getline, operator
  * 
  */
-int call_opcodes(char *op)
+void call_opcodes(stack_t **stack, char *op, unsigned int line_number)
 {
+    int i;
+
     instruction_t m_functions[] = {
         {"push", _push},
-        {"pall" _pall},
+        {"pall", _pall},
         {NULL, NULL}
 	};
 
-    
+    if (!op)
+    {
+        for (i = 0; m_functions[i].opcode; i++)
+        {
+            if (strcmp(op, m_functions[i].opcode) == 0)
+            {
+                m_functions[i].f(stack, line_number);
+                return;
+            }
+        }
 
+        if (strlen(op) != 0)
+		{
+			printf("L%u: unknown instruction %s\n", line_number, op);
+			exit(EXIT_FAILURE);
+		}
+    }
 }
